fbb0ab6d1949fb88e9327856bdc3ebcbd212293a
신희성 <vvwv3092@gmail.com>
2024-03-16 22:51:39 +0900

    init multi module

---
+++ gsmgogo-entity/src/main/java/com/example/gsmgogoentity/GsmgogoEntityApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsmgogoentity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmgogoEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GsmgogoEntityApplication.class, args);
+	}
+
+}
