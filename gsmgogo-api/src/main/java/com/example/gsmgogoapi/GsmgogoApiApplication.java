3d68f13adfaf140a73b3c315bae2ef25924b72f4
신희성 <vvwv3092@gmail.com>
2024-03-16 22:55:06 +0900

    init

---
+++ gsmgogo-api/src/main/java/com/example/gsmgogoapi/GsmgogoApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.gsmgogoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GsmgogoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GsmgogoApiApplication.class, args);
+    }
+
+}
---
+++ gsmgogo-batch/src/test/java/com/example/gsmgogobatch/GsmgogoBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsmgogobatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsmgogoBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
