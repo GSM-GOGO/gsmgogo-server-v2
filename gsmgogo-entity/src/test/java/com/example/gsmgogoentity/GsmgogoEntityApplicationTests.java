dd6b0882bcf5aec920da3f00486e2d0ffef909e4
신희성 <vvwv3092@gmail.com>
2024-03-16 23:01:52 +0900

    init multi module

---
+++ gsmgogo-entity/src/test/java/com/example/gsmgogoentity/GsmgogoEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gsmgogoentity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsmgogoEntityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
