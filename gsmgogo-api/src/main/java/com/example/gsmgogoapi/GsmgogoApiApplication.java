3792f9d2b9acf074dab274792a666bd3e1d052cc
신희성 <vvwv3092@gmail.com>
2024-03-17 00:53:25 +0900

    Ci update application scanBasePackages

--- gsmgogo-api/src/main/java/com/example/gsmgogoapi/GsmgogoApiApplication.java
+++ gsmgogo-api/src/main/java/com/example/gsmgogoapi/GsmgogoApiApplication.java
@@ -3,7 +3,7 @@ package com.example.gsmgogoapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.gsmgogoapi"})
 public class GsmgogoApiApplication {
 
     public static void main(String[] args) {
