4744c83af4a995f8d999e8a9ae8d5179a1dbdaa3
신희성 <vvwv3092@gmail.com>
2024-03-17 23:33:44 +0900

    Add AuthDetailsService

--- gsmgogo-api/src/main/java/com/example/gsmgogoapi/domain/user/repository/UserJpaRepository.java
+++ gsmgogo-api/src/main/java/com/example/gsmgogoapi/domain/user/repository/UserJpaRepository.java
@@ -3,5 +3,8 @@ package com.example.gsmgogoapi.domain.user.repository;
 import com.example.gsmgogoentity.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserJpaRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUserEmail(String userEmail);
 }
