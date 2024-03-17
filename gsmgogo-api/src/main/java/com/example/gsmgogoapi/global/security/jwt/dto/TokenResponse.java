d8b288a82c0caf3b99c0df0c6b1f3c8b508bd84a
신희성 <vvwv3092@gmail.com>
2024-03-17 23:48:06 +0900

    Add TokenResponse

---
+++ gsmgogo-api/src/main/java/com/example/gsmgogoapi/global/security/jwt/dto/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.example.gsmgogoapi.global.security.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+@Getter
+@Builder
+@AllArgsConstructor
+public class TokenResponse {
+    private final String accessToken;
+}
