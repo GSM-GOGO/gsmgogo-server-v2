df77ee6ad177ef0508fbbea54e44ab0da81c9347
신희성 <vvwv3092@gmail.com>
2024-03-18 23:32:13 +0900

    Add getToken client

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/auth/controller/AuthController.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/auth/controller/AuthController.java
@@ -5,17 +5,28 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import team.gsmgogo.global.feign.GauthClient;
+import team.gsmgogo.global.feign.dto.GauthTokenDto;
+import team.gsmgogo.global.feign.dto.GauthTokenRequest;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final GauthClient gauthClient;
+
     @Value("${gauth.clientId}")
     private String clientId;
+
+    @Value("${gauth.clientSecret}")
+    private String clientSecret;
 
     @Value("${gauth.redirectUrl}")
     private String redirectUri;
@@ -27,6 +38,16 @@ public class AuthController {
                     "redirect_uri=" + redirectUri;
 
          response.sendRedirect(loginUrl);
+    }
+
+    @GetMapping("/callback")
+    public void callback(@RequestParam("code") String code) throws URISyntaxException {
+        GauthTokenDto gauthTokenDto = gauthClient.getToken(new URI("https://server.gauth.co.kr/oauth/token"), GauthTokenRequest.builder()
+                        .code(code)
+                        .clientId("83826b547e88434eb1a6dfc27c2ba7f9d877615fc09a438c8703fbbea15a202e")
+                        .clientSecret("9e01491916754f078ddd54c37b6e2ceca5567d5e961c41d8915fb67aae90e9b8")
+                        .redirectUri("http://localhost:8080/auth/callback")
+                .build());
     }
 
 }
--- gsmgogo-api/src/main/java/team/gsmgogo/global/feign/GauthClient.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/global/feign/GauthClient.java
@@ -2,9 +2,10 @@ package team.gsmgogo.global.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import team.gsmgogo.global.config.GauthFeignConfig;
 import team.gsmgogo.global.feign.dto.GauthTokenDto;
+import team.gsmgogo.global.feign.dto.GauthTokenRequest;
 
 import java.net.URI;
 
@@ -12,9 +13,6 @@ import java.net.URI;
 public interface GauthClient {
 
     @PostMapping
-    GauthTokenDto getToken(URI baseUrl, @RequestParam("clientId") String clientId,
-                           @RequestParam("redirectUri") String redirectUrl,
-                           @RequestParam("code") String code,
-                           @RequestParam("clientSecret") String clientSecret);
+    GauthTokenDto getToken(URI baseUrl, @RequestBody GauthTokenRequest gauthTokenRequest);
 
 }
