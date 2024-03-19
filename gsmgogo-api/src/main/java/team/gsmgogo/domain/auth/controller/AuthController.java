5830c5e93954d5f8807c39238cb5fbb836eedad1
신희성 <vvwv3092@gmail.com>
2024-03-19 14:40:00 +0900

    Add UserFacade

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/auth/controller/AuthController.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/auth/controller/AuthController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import team.gsmgogo.domain.auth.controller.dto.response.AuthCallBackCodeResponse;
 import team.gsmgogo.domain.auth.controller.dto.response.TokenDto;
 import team.gsmgogo.domain.auth.service.GauthLoginService;
-import team.gsmgogo.global.feign.dto.GauthTokenDto;
+import team.gsmgogo.global.facade.UserFacade;
 import team.gsmgogo.global.manager.CookieManager;
 import team.gsmgogo.global.security.jwt.JwtTokenProvider;
 
@@ -24,6 +24,7 @@ public class AuthController {
 
     private final GauthLoginService gauthLoginService;
     private final CookieManager cookieManager;
+    private final UserFacade userFacade;
 
     @Value("${gauth.clientId}")
     private String clientId;
@@ -37,15 +38,15 @@ public class AuthController {
 
     @GetMapping("/login")
     public void login(HttpServletResponse response) throws IOException {
-         String loginUrl = "https://gauth.co.kr/login?" +
-                    "client_id=" + clientId + "&" +
-                    "redirect_uri=" + redirectUri;
+        String loginUrl = "https://gauth.co.kr/login?" +
+                "client_id=" + clientId + "&" +
+                "redirect_uri=" + redirectUri;
 
-         response.sendRedirect(loginUrl);
+        response.sendRedirect(loginUrl);
     }
 
     @GetMapping("/callback")
-    public ResponseEntity<AuthCallBackCodeResponse> callback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
+    public ResponseEntity<AuthCallBackCodeResponse> callback(@RequestParam("code") String code, HttpServletResponse response) {
         TokenDto tokenDto = gauthLoginService.execute(code);
         cookieManager.addTokenCookie(response, JwtTokenProvider.ACCESS_KEY, tokenDto.getAccessToken(), accessExp, true);
         cookieManager.addTokenCookie(response, JwtTokenProvider.REFRESH_KEY, tokenDto.getRefreshToken(), refreshExp, true);
--- gsmgogo-api/src/main/java/team/gsmgogo/global/feign/dto/GauthUserDto.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/global/feign/dto/GauthUserDto.java
@@ -2,10 +2,8 @@ package team.gsmgogo.global.feign.dto;
 
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Getter
 @NoArgsConstructor
