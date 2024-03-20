32cdba821a30c87f8fcc8881ba1ab7d598501ef1
enbraining <me@xolving.com>
2024-03-20 17:24:45 +0900

    ✨ 인증번호 인증 API Service

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/auth/service/impl/CheckVerifyCodeServiceImpl.java
@@ -0,0 +1,29 @@
+package team.gsmgogo.domain.auth.service.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.gsmgogo.domain.auth.entity.VerifyCodeRedisEntity;
+import team.gsmgogo.domain.auth.repository.VerifyCodeJpaRepository;
+import team.gsmgogo.domain.auth.service.CheckVerifyCodeService;
+import team.gsmgogo.global.exception.error.ExpectedException;
+import team.gsmgogo.global.facade.UserFacade;
+
+@Service
+@RequiredArgsConstructor
+public class CheckVerifyCodeServiceImpl implements CheckVerifyCodeService {
+    private final UserFacade userFacade;
+    private final VerifyCodeJpaRepository verifyCodeJpaRepository;
+
+    @Override
+    @Transactional
+    public void execute(String code) {
+        Long id = userFacade.getCurrentUser().getUserId();
+        VerifyCodeRedisEntity verifyCode = verifyCodeJpaRepository.findByUserId(id).get();
+
+        if(!verifyCode.getCode().equals(code)){
+            throw new ExpectedException("인증 코드가 같지 않습니다.", HttpStatus.BAD_REQUEST);
+        }
+    }
+}
