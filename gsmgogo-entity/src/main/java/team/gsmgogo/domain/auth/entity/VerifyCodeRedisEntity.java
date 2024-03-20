c3b89acc75f161998ec51e1b31d32aa28ba78b65
enbraining <me@xolving.com>
2024-03-20 10:53:10 +0900

    ✨ 인증번호 발송 API JPA

---
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/auth/entity/VerifyCodeRedisEntity.java
@@ -0,0 +1,27 @@
+package team.gsmgogo.domain.auth.entity;
+
+import jakarta.persistence.Column;
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+import org.springframework.data.redis.core.index.Indexed;
+
+@RedisHash("verifyCode")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class VerifyCodeRedisEntity {
+    @Id
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Indexed
+    @Column(name = "code")
+    private String code;
+
+    @TimeToLive
+    @Column(name = "expired_at")
+    private Long expiredAt;
+}
