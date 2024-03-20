2e8b9cade5edea02f6f46c9491d2a5f1882c48b0
Donghak Kim <me@xolving.com>
2024-03-20 15:05:22 +0900

    Update gsmgogo-api/src/main/java/team/gsmgogo/domain/auth/controller/dto/request/AuthSendCodeRequest.java
    NoArgsConstructor 추가
    
    Co-authored-by: 신희성 <131235625+tlsgmltjd@users.noreply.github.com>

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/auth/controller/dto/request/AuthSendCodeRequest.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/auth/controller/dto/request/AuthSendCodeRequest.java
@@ -3,8 +3,10 @@ package team.gsmgogo.domain.auth.controller.dto.request;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class AuthSendCodeRequest {
     @JsonProperty(value = "phone_number")
