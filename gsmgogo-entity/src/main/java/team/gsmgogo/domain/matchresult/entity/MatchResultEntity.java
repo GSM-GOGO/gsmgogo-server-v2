ec8b145ef370b21df8ef0f9e20f50a22707455cf
신희성 <vvwv3092@gmail.com>
2024-03-24 23:15:23 +0900

    Add matchresult domain

---
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/matchresult/entity/MatchResultEntity.java
@@ -0,0 +1,40 @@
+package team.gsmgogo.domain.matchresult.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import team.gsmgogo.domain.match.entity.MatchEntity;
+import team.gsmgogo.domain.team.entity.TeamEntity;
+
+@Entity
+@Table(name = "match_result")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@ToString
+public class MatchResultEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "match_result_id")
+    private Long matchResultId;
+
+    @ManyToOne
+    @PrimaryKeyJoinColumn(name = "match_id")
+    private MatchEntity match;
+
+    @ManyToOne
+    @PrimaryKeyJoinColumn(name = "team_id")
+    private TeamEntity team;
+
+    @Column(name = "team_a_score")
+    private Integer teamAScore;
+
+    @Column(name = "team_b_score")
+    private Integer teamBScore;
+
+    @Column(name = "team_a_bet")
+    private Long teamABet;
+
+    @Column(name = "team_b_bet")
+    private Long teamBBet;
+}
