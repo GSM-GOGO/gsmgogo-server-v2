c49eae1a84914ecfa79caf62617c83932e33f992
enbraining <me@xolving.com>
2024-03-27 12:19:07 +0900

    batch 실행 나누기

--- gsmgogo-api/src/main/java/team/gsmgogo/GsmgogoApiApplication.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/GsmgogoApiApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @EnableFeignClients
 @SpringBootApplication
 public class GsmgogoApiApplication {
