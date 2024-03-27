c0a52e4f044fc80bb7c7d2f0c05f73538fbef02e
enbraining <me@xolving.com>
2024-03-27 11:39:48 +0900

    batch 설정 및 정각에 실행되는 잡 추가

---
+++ gsmgogo-batch/src/main/java/team/gsmgogo/scheduler/ResetLoginCountScheduler.java
@@ -0,0 +1,42 @@
+package team.gsmgogo.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+import team.gsmgogo.domain.user.repository.UserJpaRepository;
+import team.gsmgogo.job.ResetLoginCountJob;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class ResetLoginCountScheduler {
+    private final JobLauncher jobLauncher;
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager platformTransactionManager;
+    private final UserJpaRepository userJpaRepository;
+
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void resetLoginCount() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+        Map<String, JobParameter<?>> confMap = new HashMap<>();
+        confMap.put("time", new JobParameter(System.currentTimeMillis(), String.class));
+        JobParameters jobParameters = new JobParameters(confMap);
+
+        jobLauncher.run(
+            new ResetLoginCountJob(jobRepository, platformTransactionManager, userJpaRepository).resetCountJob(),
+            jobParameters
+        );
+    }
+}
