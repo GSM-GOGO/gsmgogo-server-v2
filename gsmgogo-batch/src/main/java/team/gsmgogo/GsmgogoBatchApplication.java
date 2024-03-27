c339e7ed98a410b94df622c016398f91d6a71205
enbraining <me@xolving.com>
2024-03-27 11:46:50 +0900

    스케줄링 실행 추가

--- gsmgogo-batch/src/main/java/team/gsmgogo/GsmgogoBatchApplication.java
+++ gsmgogo-batch/src/main/java/team/gsmgogo/GsmgogoBatchApplication.java
@@ -1,11 +1,8 @@
 package team.gsmgogo;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableScheduling
 @SpringBootApplication
 public class GsmgogoBatchApplication {
 
