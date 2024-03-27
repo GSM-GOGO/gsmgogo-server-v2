5f50bb0a268ce7d75892a97cb7a9b58fb245142d
신희성 <vvwv3092@gmail.com>
2024-03-27 10:17:42 +0900

    Add TeamBadmintonSaveService

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/team/controller/dto/request/TeamBadmintonSaveRequest.java
@@ -0,0 +1,25 @@
+package team.gsmgogo.domain.team.controller.dto.request;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.gsmgogo.domain.team.enums.TeamType;
+
+import java.util.List;
+
+@NoArgsConstructor
+@Getter
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class TeamBadmintonSaveRequest {
+    @NotBlank
+    @Size(min = 1, max = 5)
+    @Pattern(regexp = "\\S{1,5}")
+    private String teamName;
+    @NotNull
+    private List<TeamParticipateDto> participates;
+}
