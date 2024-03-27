0e8bb18b560f1819be8cd8f41cfdb5e4c0cf8f93
enbraining <me@xolving.com>
2024-03-28 07:59:04 +0900

    팀 조회 API repository

--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/follow/repository/FollowJpaRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/follow/repository/FollowJpaRepository.java
@@ -2,6 +2,11 @@ package team.gsmgogo.domain.follow.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.gsmgogo.domain.follow.entity.FollowEntity;
+import team.gsmgogo.domain.team.entity.TeamEntity;
+import team.gsmgogo.domain.user.entity.UserEntity;
+
+import java.util.List;
 
 public interface FollowJpaRepository extends JpaRepository<FollowEntity, Long> {
+    boolean existsByUserAndTeam(UserEntity user, TeamEntity team);
 }
