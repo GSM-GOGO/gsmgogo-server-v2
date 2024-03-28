7f2316071987d565a78ef6306ec7aa3d5c76214c
enbraining <me@xolving.com>
2024-03-28 20:02:07 +0900

    일반 팀 상세 조회 API Repository

--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/normalteamparticipate/repository/NormalTeamParticipateJpaRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/normalteamparticipate/repository/NormalTeamParticipateJpaRepository.java
@@ -2,6 +2,10 @@ package team.gsmgogo.domain.normalteamparticipate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import team.gsmgogo.domain.normalteamparticipate.entity.NormalTeamParticipateEntity;
+import team.gsmgogo.domain.team.entity.TeamEntity;
+
+import java.util.List;
 
 public interface NormalTeamParticipateJpaRepository extends JpaRepository<NormalTeamParticipateEntity, Long> {
+    List<NormalTeamParticipateEntity> findByTeamTeamId(Long id);
 }
--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/user/repository/UserJpaRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/user/repository/UserJpaRepository.java
@@ -3,10 +3,10 @@ package team.gsmgogo.domain.user.repository;
 import team.gsmgogo.domain.user.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<UserEntity, Long> {
     Optional<UserEntity> findByUserEmail(String userEmail);
     Optional<UserEntity> findByUserId(Long userId);
-
 }
