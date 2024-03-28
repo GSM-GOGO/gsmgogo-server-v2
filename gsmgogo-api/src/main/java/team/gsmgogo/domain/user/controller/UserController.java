3839778f7e36f5518a6529b2c8dfc4063325c06d
신희성 <vvwv3092@gmail.com>
2024-03-28 16:03:01 +0900

    Add user controller

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/user/controller/UserController.java
@@ -0,0 +1,26 @@
+package team.gsmgogo.domain.user.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import team.gsmgogo.domain.user.dto.response.UserInfoResponse;
+import team.gsmgogo.domain.user.service.QueryUserInfoService;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final QueryUserInfoService queryUserInfoService;
+
+    @GetMapping
+    public ResponseEntity<List<UserInfoResponse>> queryUser(@RequestParam String name) {
+        return ResponseEntity.ok(queryUserInfoService.queryUserInfo(name));
+    }
+
+}
