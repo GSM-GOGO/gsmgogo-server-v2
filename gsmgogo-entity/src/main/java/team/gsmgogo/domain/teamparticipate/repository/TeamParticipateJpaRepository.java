247431a8dca2595da94ff3a9f90adf638f2d636b
신희성 <vvwv3092@gmail.com>
2024-03-28 19:51:31 +0900

    Update TeamListResponse

--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/teamparticipate/repository/TeamParticipateJpaRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/teamparticipate/repository/TeamParticipateJpaRepository.java
@@ -1,7 +1,12 @@
 package team.gsmgogo.domain.teamparticipate.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import team.gsmgogo.domain.team.entity.TeamEntity;
 import team.gsmgogo.domain.teamparticipate.entity.TeamParticipateEntity;
+import team.gsmgogo.domain.user.entity.UserEntity;
+
+import java.util.Optional;
 
 public interface TeamParticipateJpaRepository extends JpaRepository<TeamParticipateEntity, Long> {
+    boolean existsByUserAndTeam(UserEntity user, TeamEntity team);
 }
