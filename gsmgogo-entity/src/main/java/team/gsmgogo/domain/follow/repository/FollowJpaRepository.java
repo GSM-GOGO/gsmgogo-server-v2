9a76440a93b61e4d587e2c56605fb6516e6021f8
신희성 <vvwv3092@gmail.com>
2024-03-28 20:22:36 +0900

    Add QueryUserFollowTeamServiceImpl

--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/follow/repository/FollowJpaRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/follow/repository/FollowJpaRepository.java
@@ -6,9 +6,12 @@ import team.gsmgogo.domain.team.entity.TeamEntity;
 import team.gsmgogo.domain.user.entity.UserEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface FollowJpaRepository extends JpaRepository<FollowEntity, Long> {
     boolean existsByUserAndTeam(UserEntity user, TeamEntity team);
 
     boolean existsByUser(UserEntity user);
+
+    Optional<FollowEntity> findByUser(UserEntity user);
 }
