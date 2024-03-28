fb375169f22cca30102cb2c0e32096ceb7f73091
신희성 <vvwv3092@gmail.com>
2024-03-28 20:38:22 +0900

    Add QueryUserPointServiceImpl

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/user/service/QueryUserPointService.java
@@ -0,0 +1,7 @@
+package team.gsmgogo.domain.user.service;
+
+import team.gsmgogo.domain.user.dto.response.UserPointResponse;
+
+public interface QueryUserPointService {
+    UserPointResponse queryUserPoint();
+}
