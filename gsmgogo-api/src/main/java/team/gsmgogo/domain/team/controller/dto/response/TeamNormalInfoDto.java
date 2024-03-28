12ec20031aea8407d079d2c5c84f1ea4fc03f3a1
신희성 <vvwv3092@gmail.com>
2024-03-28 23:22:07 +0900

    Update normal team get dto

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/team/controller/dto/response/TeamNormalInfoDto.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/team/controller/dto/response/TeamNormalInfoDto.java
@@ -3,19 +3,19 @@ package team.gsmgogo.domain.team.controller.dto.response;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import team.gsmgogo.domain.user.enums.GradeEnum;
+import team.gsmgogo.domain.normalteamparticipate.enums.NormalTeamType;
 
 import java.util.List;
 
-@Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
+@Builder
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class TeamNormalListResponse {
-    private Long teamId;
-    private GradeEnum teamGrade;
-    private TeamClassType teamClass;
-    private List<TeamNormalInfoDto> teamList;
+public class TeamNormalInfoDto {
+    private NormalTeamType teamType;
+    private List<NormalTeamParticipateDto> participates;
 }
