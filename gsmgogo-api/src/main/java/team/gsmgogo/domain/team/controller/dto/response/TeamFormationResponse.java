634c1bbf5c4a4f5a7dcb12dad8fcc397946e35a7
enbraining <me@xolving.com>
2024-03-29 00:55:17 +0900

    response dto validate 추가

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/team/controller/dto/response/TeamFormationResponse.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/team/controller/dto/response/TeamFormationResponse.java
@@ -2,6 +2,9 @@ package team.gsmgogo.domain.team.controller.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,11 +20,16 @@ import java.util.List;
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TeamFormationResponse {
     private Long teamId;
+    @Enumerated(EnumType.STRING)
     private TeamType teamType;
+    @Enumerated(EnumType.STRING)
     private GradeEnum teamGrade;
+    @Enumerated(EnumType.STRING)
     private TeamClassType teamClassType;
     private boolean authorMe;
     private Integer winCount;
     private List<FormationParticipateDto> participates;
+    @Nullable
+    @Enumerated(EnumType.STRING)
     private BadmintonRank badmintonRank;
 }
