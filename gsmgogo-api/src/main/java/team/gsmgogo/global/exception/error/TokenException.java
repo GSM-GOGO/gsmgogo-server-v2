cc96267d1aebe0aa8be61460d3f2de8c5d59be5d
enbraining <me@xolving.com>
2024-03-29 21:09:12 +0900

    TokenExceptionFilter 관련 설정

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/global/exception/error/TokenException.java
@@ -0,0 +1,27 @@
+package team.gsmgogo.global.exception.error;
+
+import org.springframework.http.HttpStatus;
+
+
+public class TokenException extends RuntimeException {
+    private final HttpStatus statusCode;
+
+    public HttpStatus getStatusCode() {
+        return statusCode;
+    }
+
+    public TokenException(String message, HttpStatus statusCode) {
+        super(message);
+        this.statusCode = statusCode;
+    }
+
+    public TokenException(HttpStatus statusCode) {
+        this(statusCode.getReasonPhrase(), statusCode);
+    }
+
+    @Override
+    public synchronized Throwable fillInStackTrace() {
+        return this;
+    }
+
+}
---
+++ gsmgogo-api/src/main/java/team/gsmgogo/global/security/jwt/filter/TokenExceptionFilter.java
@@ -0,0 +1,37 @@
+package team.gsmgogo.global.security.jwt.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import team.gsmgogo.global.exception.error.ExpectedException;
+import team.gsmgogo.global.exception.error.TokenException;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class TokenExceptionFilter extends OncePerRequestFilter {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(@NotNull HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+        } catch(TokenException e){
+            httpServletResponse.setStatus(e.getStatusCode().value());
+            httpServletResponse.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            httpServletResponse.setCharacterEncoding("UTF-8");
+            ErrorObject errorObject = new ErrorObject(e.getMessage());
+
+            objectMapper.writeValue(httpServletResponse.getWriter(), errorObject);
+        } catch (ServletException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
