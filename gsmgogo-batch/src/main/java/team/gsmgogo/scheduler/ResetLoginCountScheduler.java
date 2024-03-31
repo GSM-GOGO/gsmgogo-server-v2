9bb8cf85de99cf0c6f3b10295e8e065aa56c8a16
신희성 <vvwv3092@gmail.com>
2024-04-01 01:00:57 +0900

    Update cron time

--- gsmgogo-batch/src/main/java/team/gsmgogo/scheduler/ResetLoginCountScheduler.java
+++ gsmgogo-batch/src/main/java/team/gsmgogo/scheduler/ResetLoginCountScheduler.java
@@ -28,7 +28,7 @@ public class ResetLoginCountScheduler {
     private final PlatformTransactionManager platformTransactionManager;
     private final UserQueryDslRepository userQueryDslRepository;
 
-    @Scheduled(cron = "0 30 21 * * *")
+    @Scheduled(cron = "0 5 1 * * *")
     public void resetLoginCount() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
         Map<String, JobParameter<?>> confMap = new HashMap<>();
         confMap.put("time", new JobParameter(System.currentTimeMillis(), String.class));
