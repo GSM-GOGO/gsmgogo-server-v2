e6d93ee0b7d2398c641dd1b7fb205735c1140a98
신희성 <vvwv3092@gmail.com>
2024-04-02 23:37:37 +0900

    Add team repository

--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/team/repository/TeamQueryDslRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/team/repository/TeamQueryDslRepository.java
@@ -7,20 +7,18 @@ import team.gsmgogo.domain.team.entity.QTeamEntity;
 import team.gsmgogo.domain.team.entity.TeamEntity;
 import team.gsmgogo.domain.teamparticipate.entity.QTeamParticipateEntity;
 
-import java.util.List;
-
 @Repository
 @RequiredArgsConstructor
 public class TeamQueryDslRepository {
     private final JPAQueryFactory jpaQueryFactory;
 
-    public List<TeamEntity> findByTeamId(Long timeId) {
+    public TeamEntity findByTeamId(Long timeId) {
         QTeamEntity team = QTeamEntity.teamEntity;
         QTeamParticipateEntity participate = QTeamParticipateEntity.teamParticipateEntity;
         return jpaQueryFactory.selectFrom(team)
                 .join(participate.team, team)
                 .where(team.teamId.eq(timeId))
-                .fetch();
+                .fetchOne();
     }
 
 }
