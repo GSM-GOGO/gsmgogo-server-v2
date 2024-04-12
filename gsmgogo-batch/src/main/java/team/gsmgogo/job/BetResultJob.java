a886a6552c1d9fe4d38990bedbc659f74da036e1
신희성 <vvwv3092@gmail.com>
2024-04-12 14:00:51 +0900

    Update itema reader, writer

--- gsmgogo-batch/src/main/java/team/gsmgogo/job/BetResultJob.java
+++ gsmgogo-batch/src/main/java/team/gsmgogo/job/BetResultJob.java
@@ -8,13 +8,18 @@ import org.springframework.batch.core.Step;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.data.RepositoryItemReader;
+import org.springframework.batch.item.data.builder.RepositoryItemReaderBuilder;
 import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
 import team.gsmgogo.domain.bet.entity.BetEntity;
+import team.gsmgogo.domain.bet.repository.BetJpaRepository;
 import team.gsmgogo.domain.user.entity.UserEntity;
 import team.gsmgogo.domain.user.repository.UserJpaRepository;
 
@@ -24,29 +29,36 @@ import team.gsmgogo.domain.user.repository.UserJpaRepository;
 public class BetResultJob {
     private final JobRepository jobRepository;
     private final PlatformTransactionManager platformTransactionManager;
-    private final EntityManagerFactory entityManagerFactory;
+    private final UserJpaRepository betJpaRepository;
 
     @Bean
-    public Job betResultJob(){
-        return new JobBuilder("bet-result-Job", jobRepository)
-                .start(resetCountStep(jobRepository, platformTransactionManager))
+    public Job betJob(){
+        return new JobBuilder("bet-Job", jobRepository)
+                .start(betStep(jobRepository, platformTransactionManager))
                 .build();
     }
 
     @Bean
-    public Step resetCountStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
-        return new StepBuilder("a", jobRepository)
-                .<BetEntity, BetEntity>chunk(1000, platformTransactionManager)
-                .reader(itemReader())
+    public Step betStep(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager){
+        return new StepBuilder("bet-Step", jobRepository)
+                .<UserEntity, UserEntity>chunk(5, platformTransactionManager)
+                .reader(betItemReader())
+                .writer(new ItemWriter<UserEntity>() {
+                    @Override
+                    public void write(Chunk<? extends UserEntity> chunk) throws Exception {
+                        chunk.forEach(System.out::println);
+                    }
+                })
                 .build();
     }
 
     @Bean
-    public ItemReader<BetEntity> itemReader() {
-        JpaPagingItemReader<BetEntity> reader = new JpaPagingItemReader<>();
-        reader.setEntityManagerFactory(entityManagerFactory);
-        reader.setQueryString("SELECT b FROM Bet b");
-        return reader;
+    public RepositoryItemReader<UserEntity> betItemReader() {
+        return new RepositoryItemReaderBuilder<UserEntity>()
+                .name("betReader")
+                .repository(betJpaRepository)
+                .methodName("findAll")
+                .build();
     }
 
 }
