6d2d50e5734dcb92c7edc1c633b39511ba4d2c34
enbraining <me@xolving.com>
2024-04-13 18:35:32 +0900

    :sparkles: 경기 조회 API DTO

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/match/controller/dto/response/MatchResultDto.java
@@ -0,0 +1,39 @@
+package team.gsmgogo.domain.match.controller.dto.response;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.gsmgogo.domain.match.enums.MatchLevelType;
+import team.gsmgogo.domain.match.enums.TeamClassType;
+import team.gsmgogo.domain.team.enums.TeamType;
+import team.gsmgogo.domain.user.enums.GradeEnum;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class MatchResultDto {
+    private Long matchId;
+    private TeamType matchType;
+    private MatchLevelType matchLevel;
+    private Long teamAId;
+    private String teamAName;
+    private GradeEnum teamAGrade;
+    private TeamClassType teamAClassType;
+    private Long teamBId;
+    private String teamBName;
+    private GradeEnum teamBGrade;
+    private TeamClassType teamBClassType;
+    private boolean isVote;
+    private Long teamABet;
+    private Long teamBBet;
+    private Integer teamAScore;
+    private Integer teamBScore;
+    private Integer betTeamAScore;
+    private Integer betTeamBScore;
+    private Long earnedPoint;
+    private Long losePoint;
+}
--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/bet/repository/BetJpaRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/bet/repository/BetJpaRepository.java
@@ -1,7 +1,13 @@
 package team.gsmgogo.domain.bet.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import team.gsmgogo.domain.bet.entity.BetEntity;
+import team.gsmgogo.domain.user.entity.UserEntity;
+
 
 public interface BetJpaRepository extends JpaRepository<BetEntity, Long> {
+    List<BetEntity> findByUser(UserEntity user);
 }
