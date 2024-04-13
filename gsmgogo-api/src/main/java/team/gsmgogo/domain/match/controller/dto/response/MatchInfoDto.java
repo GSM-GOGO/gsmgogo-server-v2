7e1dba191517d3a339f9bdfced42942eedc72d01
enbraining <me@xolving.com>
2024-04-13 18:38:00 +0900

    :bug: 개행문자

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/match/controller/dto/response/MatchInfoDto.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/match/controller/dto/response/MatchInfoDto.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
