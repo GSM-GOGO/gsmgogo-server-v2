73ad64678c63d4a330ec27c1a15df832d5cab1d6
신희성 <vvwv3092@gmail.com>
2024-04-15 22:35:15 +0900

    Add DailyRouletteRoll interface

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/game/controller/GameController.java
@@ -0,0 +1,4 @@
+package team.gsmgogo.domain.game.controller;
+
+public class GameController {
+}
---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/game/service/DailyRouletteRoll.java
@@ -0,0 +1,7 @@
+package team.gsmgogo.domain.game.service;
+
+import team.gsmgogo.domain.game.controller.dto.response.DailyRouletteResponse;
+
+public interface DailyRouletteRoll {
+    DailyRouletteResponse roll();
+}
