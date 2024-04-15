1ed18544611ceaa36f6b80f6a12ecf90cd454141
enbraining <me@xolving.com>
2024-04-15 12:15:05 +0900

    :recycle: validation

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/bet/controller/BetController.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/bet/controller/BetController.java
@@ -1,11 +1,13 @@
 package team.gsmgogo.domain.bet.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import team.gsmgogo.domain.bet.controller.dto.BetRequest;
 import team.gsmgogo.domain.bet.service.BetService;
 
@@ -16,7 +18,7 @@ public class BetController {
     private final BetService betService;
 
     @PostMapping
-    public ResponseEntity<Void> bet(@RequestBody BetRequest betRequest){
+    public ResponseEntity<Void> bet(@RequestBody @Valid BetRequest betRequest){
         betService.execute(betRequest);
         return ResponseEntity.ok().build();
     }
--- gsmgogo-api/src/main/java/team/gsmgogo/domain/bet/controller/dto/BetRequest.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/bet/controller/dto/BetRequest.java
@@ -1,7 +1,10 @@
 package team.gsmgogo.domain.bet.controller.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,9 +14,20 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class BetRequest {
+    @NotNull
     private Long matchId;
+    
+    @NotNull
     private Long teamId;
+    
+    @NotNull
     private Long betPoint;
+
+    @NotNull
+    @JsonProperty("team_a_score")
     private Integer teamAScore;
+    
+    @NotNull
+    @JsonProperty("team_b_score")
     private Integer teamBScore;
 }
