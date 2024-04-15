03e03b010b024b873d0d7633f7c7de9ea3fc3378
신희성 <vvwv3092@gmail.com>
2024-04-15 22:29:31 +0900

    Add Game entity

---
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/game/entity/GameEntity.java
@@ -0,0 +1,29 @@
+package team.gsmgogo.domain.game.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import team.gsmgogo.domain.user.entity.UserEntity;
+
+@Entity
+@Table(name = "game")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@ToString
+public class GameEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "game_Id")
+    private Long gameId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_b")
+    private UserEntity user;
+
+    @Column(name = "coin_toss")
+    private Integer coinToss;
+
+    @Column(name = "daily_roulette")
+    private Integer dailyRoulette;
+}
