ac09ca2c211ffbadd458c20ed5ca147d0bdab903
신희성 <vvwv3092@gmail.com>
2024-04-17 00:52:39 +0900

    Add GameController dailyRouletteRoll

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/game/controller/GameController.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/game/controller/GameController.java
@@ -1,4 +1,23 @@
 package team.gsmgogo.domain.game.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import team.gsmgogo.domain.game.controller.dto.response.DailyRouletteResponse;
+import team.gsmgogo.domain.game.service.DailyRouletteRoll;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/game")
 public class GameController {
+
+    private final DailyRouletteRoll dailyRouletteRoll;
+
+    @PostMapping("/roulette")
+    public ResponseEntity<DailyRouletteResponse> dailyRouletteRoll() {
+        return ResponseEntity.ok(dailyRouletteRoll.roll());
+    }
+
 }
