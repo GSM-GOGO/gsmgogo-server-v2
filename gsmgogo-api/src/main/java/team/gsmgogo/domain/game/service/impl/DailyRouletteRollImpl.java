b4708b0b4c86f721814b945fbed03da2601a6e23
신희성 <vvwv3092@gmail.com>
2024-04-17 00:37:37 +0900

    Add random number

--- gsmgogo-api/src/main/java/team/gsmgogo/domain/game/service/impl/DailyRouletteRollImpl.java
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/game/service/impl/DailyRouletteRollImpl.java
@@ -5,17 +5,54 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.gsmgogo.domain.game.controller.dto.response.DailyRouletteResponse;
 import team.gsmgogo.domain.game.service.DailyRouletteRoll;
+import team.gsmgogo.domain.user.entity.UserEntity;
+import team.gsmgogo.domain.user.repository.UserJpaRepository;
+import team.gsmgogo.global.facade.UserFacade;
+
+import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
 public class DailyRouletteRollImpl implements DailyRouletteRoll {
 
+    private final UserJpaRepository userJpaRepository;
+    private final UserFacade userFacade;
 
     @Override
     @Transactional
     public DailyRouletteResponse roll() {
 
-        return null;
+        Random random = new Random();
+        int randomNumber = random.nextInt(6) + 1;
 
+        UserEntity currentUser = userFacade.getCurrentUser();
+        int earnedPoint;
+
+        switch (randomNumber) {
+            case 1: {
+                earnedPoint = 2000;
+                break;
+            }
+            case 2: {
+                earnedPoint = 100;
+                break;
+            }
+            case 3: {
+                earnedPoint = 500;
+                break;
+            }
+            case 4: {
+                earnedPoint = 100;
+                break;
+            }
+            case 5: {
+                earnedPoint = 500;
+                break;
+            }
+            case 6: {
+                earnedPoint = 2000;
+                break;
+            }
+        }
     }
 }
