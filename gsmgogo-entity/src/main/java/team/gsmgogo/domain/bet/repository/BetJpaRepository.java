ace004c8a22cbf398499875bfc3b5acdd75830a7
신희성 <vvwv3092@gmail.com>
2024-04-23 12:17:31 +0900

    Update existsByUserAndMatch lock

--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/bet/repository/BetJpaRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/bet/repository/BetJpaRepository.java
@@ -1,8 +1,10 @@
 package team.gsmgogo.domain.bet.repository;
 
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import team.gsmgogo.domain.bet.entity.BetEntity;
@@ -16,5 +18,6 @@ import java.util.Optional;
 public interface BetJpaRepository extends JpaRepository<BetEntity, Long> {
     Page<BetEntity> findByMatch(MatchEntity match, Pageable pageable);
     List<BetEntity> findByUser(UserEntity user);
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     boolean existsByUserAndMatch(UserEntity user, MatchEntity matchEntity);
 }
