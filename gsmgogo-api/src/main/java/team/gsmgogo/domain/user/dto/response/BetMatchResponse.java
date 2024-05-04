b7bac672f837e2f69b3af1fe179e0b77476ab5f4
enbraining <me@xolving.com>
2024-05-04 15:58:05 +0900

    프로필 매치 조회 DTO

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/user/dto/response/BetMatchResponse.java
@@ -0,0 +1,19 @@
+package team.gsmgogo.domain.user.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class BetMatchResponse {
+    private List<BetMatchInfoDto> matches;
+    private List<BetMatchResultDto> matchResult;
+}
