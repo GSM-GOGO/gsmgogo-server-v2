5edf939f6ac993289f84015794e1d08cc63256ba
신희성 <vvwv3092@gmail.com>
2024-05-05 13:19:29 +0900

    Add coin count controller

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/game/controller/dto/response/CoinCountResponse.java
@@ -0,0 +1,10 @@
+package team.gsmgogo.domain.game.controller.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CoinCountResponse {
+    private Integer count;
+}
