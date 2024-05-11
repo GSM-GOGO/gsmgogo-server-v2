8fc5d59429fdd9a5768e577b5ad351eedaf445ef
신희성 <vvwv3092@gmail.com>
2024-05-12 04:18:32 +0900

    Add ButtonGameStateService

---
+++ gsmgogo-api/src/main/java/team/gsmgogo/domain/game/service/ButtonGameStateService.java
@@ -0,0 +1,7 @@
+package team.gsmgogo.domain.game.service;
+
+import team.gsmgogo.domain.game.controller.dto.response.ButtonGameResponse;
+
+public interface ButtonGameStateService {
+    ButtonGameResponse execute();
+}
