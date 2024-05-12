e0784aff27b22c9063f168979ee5846ac96a4723
신희성 <vvwv3092@gmail.com>
2024-05-12 23:14:15 +0900

    Update ButtonGameQueryDslRepository

--- gsmgogo-entity/src/main/java/team/gsmgogo/domain/buttongame/repository/ButtonGameQueryDslRepository.java
+++ gsmgogo-entity/src/main/java/team/gsmgogo/domain/buttongame/repository/ButtonGameQueryDslRepository.java
@@ -1,18 +1,11 @@
 package team.gsmgogo.domain.buttongame.repository;
 
-import com.querydsl.core.types.Projections;
-import com.querydsl.core.types.dsl.EnumPath;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import static team.gsmgogo.domain.buttongame.entity.QButtonGameEntity.buttonGameEntity;
 
 import team.gsmgogo.domain.buttongame.entity.ButtonGameEntity;
-import team.gsmgogo.domain.buttongame.enums.ButtonType;
-import team.gsmgogo.domain.buttongame.repository.dto.ButtonGameQueryDto;
-import team.gsmgogo.domain.user.entity.UserEntity;
-
-import java.util.Arrays;
 
 import static team.gsmgogo.domain.buttongameparticipate.entity.QButtonGameParticipate.buttonGameParticipate;
 
@@ -25,9 +18,9 @@ public class ButtonGameQueryDslRepository {
         return queryFactory
                 .selectFrom(buttonGameEntity)
                 .join(buttonGameEntity.participates, buttonGameParticipate).fetchJoin()
-                .where(buttonGameEntity.createDate.month().eq(month)
+                .where(
+                        buttonGameEntity.createDate.month().eq(month)
                         .and(buttonGameEntity.createDate.dayOfMonth().eq(day))
-                        .and(buttonGameParticipate.user.eq(user))
                 )
                 .fetchOne();
     }
